5ef434b4b91e2d718e2fbdc0ef7d84970f5b7c88
unknown <rado7711@yahoo.co.uk>
2021-06-20 10:29:40 +0100

    Style changes

--- styles/script.js
+++ styles/script.js
@@ -1,11 +1,13 @@
 const pics = document.querySelectorAll('.card')
 
+
+
 pics.forEach((pic) => {
     pic.addEventListener('click', () => {
         pic.classList.add('active')
-
-
+       
     })
+    
 })
 
 pics.forEach((pic) => {
